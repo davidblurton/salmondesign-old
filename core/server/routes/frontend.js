23f0ab121e15d6c720f5c5e7875e9ef4115895ed
David Blurton <david.blurton@gmail.com>
2014-03-21 17:34:56 +0000

    Use static homepage

--- core/server/routes/frontend.js
+++ core/server/routes/frontend.js
@@ -1,14 +1,19 @@
-var frontend    = require('../controllers/frontend');
+var frontend = require('../controllers/frontend');
 
 module.exports = function (server) {
-    /*jslint regexp: true */
+  /*jslint regexp: true */
 
-    // ### Frontend routes
-    server.get('/rss/', frontend.rss);
-    server.get('/rss/:page/', frontend.rss);
-    server.get('/tag/:slug/page/:page/', frontend.tag);
-    server.get('/tag/:slug/', frontend.tag);
-    server.get('/page/:page/', frontend.homepage);
-    server.get('/', frontend.homepage);
-    server.get('*', frontend.single);
+  // ### Frontend routes
+  server.get('/rss/', frontend.rss);
+  server.get('/rss/:page/', frontend.rss);
+  server.get('/tag/:slug/page/:page/', frontend.tag);
+  server.get('/tag/:slug/', frontend.tag);
+  server.get('/page/:page/', frontend.homepage);
+  server.get('/portfolio/', frontend.homepage);
+  server.get('/', function (req, res, next) {
+    var redirect = {};
+    redirect.path = '/index/';
+    return frontend.single(redirect, res, next);
+  });
+  server.get('*', frontend.single);
 };
